2cf1653207383ac5d05c1eccf5d59217406b12a0
ShikaCodes <synthetic.simplistic@gmail.com>
2025-03-13 19:20:11 -0700

    created basic code to pull text from the book file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+
+def main():
+    test_print = get_book_text("books/frankenstein.txt")
+    print (test_print)
+
+
+if __name__ == "__main__":
+    main()
