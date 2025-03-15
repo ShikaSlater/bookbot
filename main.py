4621630bbf3bf896397b6d2004d419a9b5d07840
ShikaCodes <synthetic.simplistic@gmail.com>
2025-03-15 10:06:29 -0700

    Added a character counter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 from stats import get_word_count
+from stats import get_character_count
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,10 +8,15 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    test_print = get_book_text("books/frankenstein.txt")
-    wc = get_word_count("books/frankenstein.txt")
-    print (f"{wc} words found in the document")
+    path = "~/bookbot-1/books/frankenstein.txt"
+    expanded_path = os.path.expanduser(path)
+    test_print = get_book_text(expanded_path)
+    wc = get_word_count(expanded_path)
+    cc = get_character_count(expanded_path)
     print (test_print)
+    print (f"{wc} words found in the document")
+    print (cc)
+    
 
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,6 +1,16 @@
-def get_word_count(filepath):
-    with open(filepath) as f:
+def get_word_count(text_file):
+    with open(text_file, 'r') as f:
         file_contents = f.read()
     count = file_contents.split()
     word_count = len(count)
     return word_count
+
+def get_character_count(text_file):
+    with open(text_file, 'r') as f:
+        file_contents = f.read()
+    char_counts = {}
+    for char in file_contents:
+            if char.isalpha():
+                char = char.lower()
+            char_counts[char] = char_counts.get(char, 0) + 1
+    return char_counts
